b5d68b6b1e4a2a2cd09ab89dd7a8e8f8aba25d73
Jose <jocvegar@gmail.com>
2020-06-15 19:56:47 -0600

    initial commit

---
+++ src/components/Menu.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="menu container">
+    <div class="row">
+      <div
+        v-for="item in menuItems"
+        :key="item.id"
+        class="col-12 col-sm-12 col-md-6"
+      >
+        <div class="card-deck">
+          <div class="card mb-3 bg-light">
+            <div class="media">
+              <img
+                :src="item.image"
+                class="align-self-center mr-3 menu-thumbnail img-thumbnail"
+                :alt="item.title"
+              />
+              <div class="media-body">
+                <h5 class="mt-3">
+                  <u>{{ item.title }}</u>
+                </h5>
+                <p class="text-left">
+                  Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+                  |<small class="text-muted"> Lps. 100</small>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data: function() {
+    return {
+      menuItems: [
+        {
+          id: 1,
+          title: "Cerdo Glaceado",
+          image: require("../assets/image-1.jpg")
+        },
+        {
+          id: 2,
+          title: "Pasta Fresca",
+          image: require("../assets/image-2.jpg")
+        },
+        { id: 3, title: "Burritos", image: require("../assets/image-3.jpg") },
+        { id: 4, title: "Panqueques", image: require("../assets/image-4.jpg") }
+      ]
+    };
+  }
+};
+</script>
+<style scoped>
+.menu-thumbnail {
+  width: 8em;
+  height: 8em;
+}
+
+img {
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,59 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="jumbotron jumbotron-fluid bg-cover text-white">
+      <div class="container">
+        <h1 class="display-4">MENU</h1>
+        <h4 class="font-weight-bolder mt-5 text-capitalize">
+          Presenta tu menu de forma digital
+        </h4>
+      </div>
+    </div>
+    <Menu />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Menu from "@/components/Menu.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    Menu
   }
 };
 </script>
+<style scoped>
+.jumbotron {
+  background-image: linear-gradient(
+      to bottom,
+      rgba(0, 0, 0, 0.6) 0%,
+      rgba(0, 0, 0, 0.6) 100%
+    ),
+    url("https://source.unsplash.com/GXXYkSwndP4/1600x900");
+}
+.bg-cover {
+  background-attachment: static;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+@media (min-width: 768px) {
+  .jumbotron {
+    height: 50vh;
+  }
+}
+
+@media (min-width: 992px) {
+  .jumbotron {
+    height: 60vh;
+  }
+}
+
+@media (min-width: 1200px) {
+  .jumbotron {
+    height: 70vh;
+  }
+}
+</style>
